f4e30de875001fb1432a25c2f2d266b5832bfdd7
Raul <raulraul.sp@gmail.com>
2022-03-09 23:06:27 +0100

    Proyecto SanchezPerezRaul2122 comentado con javadoc

--- src/jamon/Main.java
+++ src/jamon/Main.java
@@ -3,12 +3,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package jamon;
-
-/**
+/** 
+ * Clase main del programa, que define un objeto Jamon con unos parámetros 
+ * y realiza  acciones de comprar y vender jamones
  *
  * @author portatil_profesorado
  */
 public class Main {
+    
+    /** * Método main.
+     * 
+     * Este método es el principal de ejecución del programa
+     *
+     */
     
     public static void main(String[] args) {
         JamonSanchezPerezRaul2122 miJamonSanchezPerezRaul2122;
@@ -21,7 +28,21 @@ public class Main {
         stockActual = miJamonSanchezPerezRaul2122.obtenerStock();
         System.out.println("El stock actual es "+ stockActual );
     }
-
+    
+ /**
+* Método compraJamSanchezPerezRaul2122.
+* 
+* En este método se va a realizar la acción de comprar jamones
+* se hace con la idea de comprobar que no se compran más jamones
+ * de los que hay en stock ni que se compre
+ *un número negativo de jamones
+* 
+* @param cantidad número de jamones que se van a comprar
+*
+* @throws Exception si la cantidad de jamones son negativos
+ * o cero, si hay menos jamones en stock de los que se quieren vender
+*/
+    
     private static void compraJamSanchezPerezRaul2122(JamonSanchezPerezRaul2122 miJamonSanchezPerezRaul2122) {
         try
         {
@@ -33,6 +54,20 @@ public class Main {
         }
     }
 
+ /**
+* Método vendeJamSanchezPerezRaul2122.
+* 
+* En este método se va a realizar la acción de vender jamones
+* se hace con la idea de comprobar que no se vendan más jamones
+ * de los que hay en stock ni que se venda
+ *un número negativo de jamones
+* 
+* @param cantidad número de jamones que se van a vender
+*
+* @throws Exception si la cantidad de jamones son negativos
+ * o cero, si hay menos jamones en stock de los que se quieren vender
+*/
+    
     private static void vendeJamSanchezPerezRaul2122(JamonSanchezPerezRaul2122 miJamonSanchezPerezRaul2122) {
         try
         {
